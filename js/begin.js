874eb1ebd8118cc3f303ec1ae72f6cdaa8bee0b1
Tanmay Gurav <tanmayvgurav@gmail.com>
2024-05-06 11:19:13 +0530

    init

--- js/begin.js
+++ js/begin.js
@@ -5,33 +5,15 @@ const avatar= document.getElementById("avatar")
 
 
 const onloaded=()=>{
-    
-    app.style.backgroundImage='url(./img/begin.svg)'
-    app.innerHTML=
-    `
-    <div class="start-container">
-        <div id="start-btn" class="btn" onlick="start()">
-            <div class="btn-text">START</div>
-        </div>
-    </div>
-    `
+    // app.innerHTML=``
+    // app.style.backgroundImage='url(./img/begin.svg)'
+    // app.innerHTML+=
+    // `
+    // <div class="start-container">
+    //     <div id="start-btn" class="btn" onlick="start()">
+    //         <div class="btn-text">START</div>
+    //     </div>
+    // </div>
+    // `
 }
 
-/* const start = () => {
-    // clear laptop background
-    app.innerHTML=''
-    app.style.backgroundImage=''
-    // add avatar
-    avatar.style.display='block'
-
-} */
-
-$(document).ready(function(){
-    $('#start-btn').click(function (e) { 
-        e.preventDefault();
-        console.log("clickd");
-        $('#APP').css('background-image',"").empty();
-        $('#avatar').fadeIn(1000);
-    });
-
-})
